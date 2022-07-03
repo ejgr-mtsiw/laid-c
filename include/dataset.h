2e37257234686147e1dfd122a4efe7b42d5d7f6e
Eduardo Ribeiro <1600820@estudante.uab.pt>
2022-07-03 15:24:50 +0100

    Dataset doesn't need to know where the data is stored

--- include/dataset.h
+++ include/dataset.h
@@ -11,10 +11,10 @@
 
 #include "bit_utils.h"
 #include "globals.h"
-#include "hdf5.h"
 #include <math.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
